9753b2347c17bd3dc47d56479d3f42715f1deeb4
Juan Adhiasta Pratama <juanadhiastapersonal@gmail.com>
2024-05-22 03:22:08 +0700

    First commit, making product page

---
+++ gen-20-exercise-react-basic/create-basic/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ProductPage from './pages/ProductPage'
+
+function App() {
+
+  return (
+    <>
+      <body className='h-screen'>
+        <ProductPage />
+      </body>
+    </>
+  )
+}
+
+export default App
