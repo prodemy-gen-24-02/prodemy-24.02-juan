44b3a978f746ef19b55d7b7775690e35ea660455
Juan Adhiasta Pratama <juanadhiastapersonal@gmail.com>
2024-06-06 14:37:36 +0700

    First commit, lesson object array

---
+++ gen-20-array-manipulation/array.js
@@ -0,0 +1,108 @@
+let cards = [
+    {
+        "id": 1,
+        "name": "Ancient Gear Advance",
+        "type": "Spell",
+        "set_code": "LEDE-EN055",
+        "image_url": "https://images.ygoprodeck.com/images/cards/4064925.jpg",
+        "price": "0.15"
+    },
+    {
+        "id": 2,
+        "name": "Ancient Gear Commander",
+        "type": "Effect Monster",
+        "atk": 500,
+        "def": 2000,
+        "level": 4,
+        "race": "Machine",
+        "attribute": "EARTH",
+        "set_code": "LEDE-EN008",
+        "image_url": "https://images.ygoprodeck.com/images/cards/27483935.jpg",
+        "price": "0.14"
+    },
+    {
+        "id": 3,
+        "name": "Ancient Gear Dark Golem",
+        "type": "Effect Monster",
+        "atk": 3000,
+        "def": 3000,
+        "level": 8,
+        "race": "Machine",
+        "attribute": "EARTH",
+        "set_code": "LEDE-EN006",
+        "image_url": "https://images.ygoprodeck.com/images/cards/64603182.jpg",
+        "price": "0.29"
+    },
+    {
+        "id": 4,
+        "name": "Ancient Gear Duel",
+        "type": "Trap Card",
+        "set_code": "LEDE-EN072",
+        "image_url": "https://images.ygoprodeck.com/images/cards/53541822.jpg",
+        "price": "0.13"
+    },
+    {
+        "id": 5,
+        "name": "Ancient Gear Tanker",
+        "type": "Effect Monster",
+        "atk": 1300,
+        "def": 1300,
+        "level": 4,
+        "race": "Machine",
+        "attribute": "EARTH",
+        "set_code": "LEDE-EN007",
+        "image_url": "https://images.ygoprodeck.com/images/cards/91098230.jpg",
+        "price": "0.16"
+    }
+
+]
+
+function addCards(card) {
+    cards.push(card); //add object at the end
+    console.log('Menambah push: ',cards)
+}
+
+function addStartCard(card) {
+    cards.unshift(card); // object at the start
+    console.log('Menambah unshift: ',cards)
+}
+
+function addSplice(start, manyItem, card){
+    cards.splice(start, manyItem, card); //object in the middle, start = index mulai, manyItem = banyak item yang dihilangkan, card = object
+    console.log('Menambah splice',cards)
+}
+
+function removeCardById(id){
+    cards = cards.filter(card => card.id !==id)
+    console.log('Menghapus by id: ',cards)
+}
+
+function editCardById(id, update){
+    cards = cards.map( card => {
+        if (card.id === id) {
+            return {...card, ...update}
+        }
+        return card;
+
+    });
+    console.log('Update by id: ',cards)
+}
+
+const payload = {
+    "id": 6,
+    "name": "Ancient Gear Advance SIR",
+    "type": "Spell",
+    "set_code": "LEDE-EN055",
+    "image_url": "https://images.ygoprodeck.com/images/cards/4064925.jpg",
+    "price": "0.15"
+}
+
+const payloadUpdate = {
+    name: "Ancient Gear Duel LINKZ"
+}
+
+addCards(payload); //works
+
+editCardById(4, payloadUpdate); //works
+
+removeCardById(2); //works
---
+++ gen-20-array-manipulation/objectArray.js
@@ -0,0 +1,293 @@
+const cardObject = [
+    {
+        "id": "4064925",
+        "name": "Ancient Gear Advance",
+        "type": "Spell Card",
+        "frameType": "spell",
+        "desc": "When this card is activated: Add 1 \"Ancient Gear\" Spell/Trap from your Deck to your hand, except \"Ancient Gear Advance\". Once per turn: You can Tribute 1 monster; draw 1 card and apply this effect for the rest of this turn (even if this card leaves the field).\r\n● You can Normal Summon \"Ancient Gear Golem\", or Level 5 or higher monsters that mention it, without Tributing.\r\nYou cannot Set cards the turn you activate this card. You can only activate 1 \"Ancient Gear Advance\" per turn.",
+        "race": "Continuous",
+        "archetype": "Ancient Gear",
+        "ygoprodeck_url": "https://ygoprodeck.com/card/ancient-gear-advance-14294",
+        "card_sets": [
+            {
+                "set_name": "Legacy of Destruction",
+                "set_code": "LEDE-EN055",
+                "set_rarity": "Common",
+                "set_rarity_code": "(C)",
+                "set_price": "0"
+            }
+        ],
+        "card_images": [
+            {
+                "id": 4064925,
+                "image_url": "https://images.ygoprodeck.com/images/cards/4064925.jpg",
+                "image_url_small": "https://images.ygoprodeck.com/images/cards_small/4064925.jpg",
+                "image_url_cropped": "https://images.ygoprodeck.com/images/cards_cropped/4064925.jpg"
+            }
+        ],
+        "card_prices": [
+            {
+                "cardmarket_price": "0.09",
+                "tcgplayer_price": "0.15",
+                "ebay_price": "0.00",
+                "amazon_price": "0.00",
+                "coolstuffinc_price": "0.00"
+            }
+        ]
+    },
+    {
+        "id": "27483935",
+        "name": "Ancient Gear Commander",
+        "type": "Effect Monster",
+        "frameType": "effect",
+        "desc": "You can send 1 \"Ancient Gear Golem\" from your hand, Deck, or face-up Monster Zone to the GY; immediately after this effect resolves, Normal Summon 1 \"Ancient Gear\" monster. If you Normal or Special Summon \"Ancient Gear Golem\" (except during the Damage Step): You can Special Summon 1 \"Ancient Gear Golem\" from your hand or GY, ignoring its Summoning conditions. You can banish this card from your GY; place 1 \"Ancient Gear\" Continuous Trap from your hand face-up on your field. You can only use each effect of \"Ancient Gear Commander\" once per turn.",
+        "atk": 500,
+        "def": 2000,
+        "level": 4,
+        "race": "Machine",
+        "attribute": "EARTH",
+        "archetype": "Ancient Gear",
+        "ygoprodeck_url": "https://ygoprodeck.com/card/ancient-gear-commander-14295",
+        "card_sets": [
+            {
+                "set_name": "Legacy of Destruction",
+                "set_code": "LEDE-EN008",
+                "set_rarity": "Common",
+                "set_rarity_code": "(C)",
+                "set_price": "0"
+            }
+        ],
+        "card_images": [
+            {
+                "id": 27483935,
+                "image_url": "https://images.ygoprodeck.com/images/cards/27483935.jpg",
+                "image_url_small": "https://images.ygoprodeck.com/images/cards_small/27483935.jpg",
+                "image_url_cropped": "https://images.ygoprodeck.com/images/cards_cropped/27483935.jpg"
+            }
+        ],
+        "card_prices": [
+            {
+                "cardmarket_price": "0.08",
+                "tcgplayer_price": "0.14",
+                "ebay_price": "0.00",
+                "amazon_price": "0.00",
+                "coolstuffinc_price": "0.00"
+            }
+        ]
+    },
+    {
+        "id": "64603182",
+        "name": "Ancient Gear Dark Golem",
+        "type": "Effect Monster",
+        "frameType": "effect",
+        "desc": "This card's name becomes \"Ancient Gear Golem\" while on the field or in the GY. If this card is Normal or Special Summoned: You can add up to 2 cards (\"Ancient Gear\" cards and/or \"Geartown\") from your Deck to your hand, except \"Ancient Gear Dark Golem\", then discard 1 card, also you cannot Set cards for the rest of this turn. You can only use this effect of \"Ancient Gear Dark Golem\" once per turn. If this card attacks, your opponent cannot activate Spell/Trap Cards until the end of the Damage Step.",
+        "atk": 3000,
+        "def": 3000,
+        "level": 8,
+        "race": "Machine",
+        "attribute": "EARTH",
+        "archetype": "Ancient Gear",
+        "ygoprodeck_url": "https://ygoprodeck.com/card/ancient-gear-dark-golem-14296",
+        "card_sets": [
+            {
+                "set_name": "Legacy of Destruction",
+                "set_code": "LEDE-EN006",
+                "set_rarity": "Super Rare",
+                "set_rarity_code": "(SR)",
+                "set_price": "0"
+            }
+        ],
+        "card_images": [
+            {
+                "id": 64603182,
+                "image_url": "https://images.ygoprodeck.com/images/cards/64603182.jpg",
+                "image_url_small": "https://images.ygoprodeck.com/images/cards_small/64603182.jpg",
+                "image_url_cropped": "https://images.ygoprodeck.com/images/cards_cropped/64603182.jpg"
+            }
+        ],
+        "card_prices": [
+            {
+                "cardmarket_price": "0.22",
+                "tcgplayer_price": "0.29",
+                "ebay_price": "0.00",
+                "amazon_price": "0.00",
+                "coolstuffinc_price": "0.00"
+            }
+        ]
+    },
+    {
+        "id": "53541822",
+        "name": "Ancient Gear Duel",
+        "type": "Trap Card",
+        "frameType": "trap",
+        "desc": "\"Ancient Gear Golem\", and monsters that mention it, in your Monster Zone are unaffected by your opponent's activated monster effects. If your opponent controls a monster: You can Fusion Summon 1 Fusion Monster that mentions \"Ancient Gear Golem\" from your Extra Deck, by banishing monsters from your field or GY as material, including \"Ancient Gear Golem\" you control, and if you do, it can make up to 3 attacks during each Battle Phase. You can only use this effect of \"Ancient Gear Duel\" once per turn.",
+        "race": "Continuous",
+        "archetype": "Ancient Gear",
+        "ygoprodeck_url": "https://ygoprodeck.com/card/ancient-gear-duel-14297",
+        "card_sets": [
+            {
+                "set_name": "Legacy of Destruction",
+                "set_code": "LEDE-EN072",
+                "set_rarity": "Common",
+                "set_rarity_code": "(C)",
+                "set_price": "0"
+            }
+        ],
+        "card_images": [
+            {
+                "id": 53541822,
+                "image_url": "https://images.ygoprodeck.com/images/cards/53541822.jpg",
+                "image_url_small": "https://images.ygoprodeck.com/images/cards_small/53541822.jpg",
+                "image_url_cropped": "https://images.ygoprodeck.com/images/cards_cropped/53541822.jpg"
+            }
+        ],
+        "card_prices": [
+            {
+                "cardmarket_price": "0.09",
+                "tcgplayer_price": "0.13",
+                "ebay_price": "0.00",
+                "amazon_price": "0.00",
+                "coolstuffinc_price": "0.00"
+            }
+        ]
+    },
+    {
+        "id": "91098230",
+        "name": "Ancient Gear Tanker",
+        "type": "Effect Monster",
+        "frameType": "effect",
+        "desc": "If this card is Normal or Special Summoned: You can Special Summon 1 \"Ancient Gear\" monster from your hand, except \"Ancient Gear Tanker\", ignoring its Summoning conditions, or, if your opponent controls a monster, you can Special Summon from your GY instead. You can target 1 face-up card you control; destroy it, also for the rest of this turn, all monsters you control that are \"Ancient Gear Golem\", or mention it, gain 600 ATK. You can only use each effect of \"Ancient Gear Tanker\" once per turn.",
+        "atk": 1300,
+        "def": 1300,
+        "level": 4,
+        "race": "Machine",
+        "attribute": "EARTH",
+        "archetype": "Ancient Gear",
+        "ygoprodeck_url": "https://ygoprodeck.com/card/ancient-gear-tanker-14298",
+        "card_sets": [
+            {
+                "set_name": "Legacy of Destruction",
+                "set_code": "LEDE-EN007",
+                "set_rarity": "Common",
+                "set_rarity_code": "(C)",
+                "set_price": "0"
+            }
+        ],
+        "card_images": [
+            {
+                "id": 91098230,
+                "image_url": "https://images.ygoprodeck.com/images/cards/91098230.jpg",
+                "image_url_small": "https://images.ygoprodeck.com/images/cards_small/91098230.jpg",
+                "image_url_cropped": "https://images.ygoprodeck.com/images/cards_cropped/91098230.jpg"
+            }
+        ],
+        "card_prices": [
+            {
+                "cardmarket_price": "0.10",
+                "tcgplayer_price": "0.16",
+                "ebay_price": "0.00",
+                "amazon_price": "0.00",
+                "coolstuffinc_price": "0.00"
+            }
+        ]
+    },
+    {
+        "id": "56208713",
+        "name": "Bacha the Melodious Maestra",
+        "type": "Fusion Monster",
+        "frameType": "fusion",
+        "desc": "2 \"Melodious\" monsters\r\nWhile you control this Fusion Summoned card, the activated effects of your \"Melodious\" Fusion Monsters cannot be negated. You can only use each of the following effects of \"Bacha the Melodious Maestra\" once per turn. If this card is Special Summoned: You can Special Summon 1 \"Melodious\" monster from your Deck. If this card is sent to the GY: You can target 1 \"Melodious\" monster in your GY, except \"Bacha the Melodious Maestra\"; Special Summon it in Defense Position.",
+        "atk": 2500,
+        "def": 1800,
+        "level": 7,
+        "race": "Fairy",
+        "attribute": "LIGHT",
+        "archetype": "Melodious",
+        "ygoprodeck_url": "https://ygoprodeck.com/card/bacha-the-melodious-maestra-14284",
+        "card_sets": [
+            {
+                "set_name": "Legacy of Destruction",
+                "set_code": "LEDE-EN035",
+                "set_rarity": "Common",
+                "set_rarity_code": "(C)",
+                "set_price": "0"
+            }
+        ],
+        "card_images": [
+            {
+                "id": 56208713,
+                "image_url": "https://images.ygoprodeck.com/images/cards/56208713.jpg",
+                "image_url_small": "https://images.ygoprodeck.com/images/cards_small/56208713.jpg",
+                "image_url_cropped": "https://images.ygoprodeck.com/images/cards_cropped/56208713.jpg"
+            }
+        ],
+        "card_prices": [
+            {
+                "cardmarket_price": "0.13",
+                "tcgplayer_price": "0.11",
+                "ebay_price": "0.00",
+                "amazon_price": "0.00",
+                "coolstuffinc_price": "0.00"
+            }
+        ]
+    }
+]
+
+function addCards(card) {
+    cardObject.push(card); //add object at the end
+    console.log(cardObject)
+}
+
+function addStartCard(card) {
+    cardObject.unshift(card); // object at the start
+    console.log(cardObject)
+}
+
+function addSplice(start, manyItem, card) {
+    cardObject.splice(start, manyItem, card); //object in the middle, start = index mulai, manyItem = banyak item yang dihilangkan, card = object
+    console.log(cardObject)
+}
+
+function removeCardById(id) {
+    cardObject = cardObject.filter(card => card.id !== id)
+    console.log(cardObject)
+}
+
+function editCardById(id, update) {
+    cardObject = cardObject.map(card => {
+        if (card.id === id) {
+            return { ...card, ...update }
+        }
+        return card;
+
+    });
+    console.log('Updated by id: ', cardObject)
+}
+
+function nestedEditById(cardId, propName, nestedId, updatedData){
+    cardObject = cardObject.map(card => {
+        if(card.id === cardId && card[propName]){
+            const updateNestedArray = card[propName].map(item => {
+                if(item.id === nestedId || item.set_code === nestedId){
+                    return {...item, ...updatedData}
+                }
+                return item
+            })
+            return {...card, [propName]: updateNestedArray}
+        }
+        return card;
+    })
+}
+
+const payload = {
+    "id": 6,
+    "name": "Ancient Gear Advance SIR",
+    "type": "Spell",
+    "set_code": "LEDE-EN055",
+    "image_url": "https://images.ygoprodeck.com/images/cards/4064925.jpg",
+    "price": "0.15"
+}
+
+const payloadUpdate = {
+    name: "Ancient Gear Duel LINKZ"
+}
